74691ea2535acdae6b6441af84844d8951c725da
Bulent Aytekin <bulentaytekin@gmail.com>
2024-01-31 02:44:01 +0300

    Km To Mil metodu eklendi ve kullanildi

---
+++ app/src/main/java/com/example/bootcamptfonksiyonlar/FonksiyonOdevleri.kt
@@ -0,0 +1,15 @@
+package com.example.bootcamptfonksiyonlar
+
+class FonksiyonOdevleri {
+
+ //Parametre olarak girilen kilometreyi mil'e dönüştürdükten sonra geri döndüren metot (mil = km*0.621)
+    fun kmToMil(km:Int):Double{
+
+        return km*0.621
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/bootcamptfonksiyonlar/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bootcamptfonksiyonlar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val odev=FonksiyonOdevleri()
+
+        //kilometreyi mil'e dönüştürüyoruz
+       val mil= odev.kmToMil(520)
+        println("Km to Mil :$mil")
+
+
+    }
+}
